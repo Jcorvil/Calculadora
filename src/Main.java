e84521614f82e1fe400e6a405e31d47ed1ec6014
Jorge <jcorvil600@g.educaand.es>
2022-11-17 12:59:08 +0100

    Hecho con un Switch

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,30 @@ public class Main {
     //Calculadora con suma, resta, división y multiplicación.
     final static Integer a = 2;
     final static Integer b = 3;
+    enum Operacion{
+        suma, resta, division, multiplicacion
+    }
 
     public static void main(String[] args) {
-
         CalculadoraBasica basica = new CalculadoraBasica();
+        Operacion operacionSeleccionada = Operacion.multiplicacion;
 
-        Integer resultadoSuma = basica.suma(a, b);
-        System.out.println("La suma de " + a + " + " + b + " = " + basica.suma(a,b));
+        switch(operacionSeleccionada) {
+            case suma:
+                System.out.println("La suma de " + a + " + " + b + " = " + basica.suma(a, b));
+                break;
 
-        Integer resultadoResta = basica.resta(a, b);
-        System.out.println("La resta de " + a + " - " + b + " = " + basica.resta(a,b));
+            case resta:
+                System.out.println("La resta de " + a + " - " + b + " = " + basica.resta(a, b));
+                break;
 
-        Integer resultadoDivision = basica.division(a, b);
-        System.out.println("La disivion de " + a + " / " + b + " = " + basica.division(a,b));
+            case division:
+                System.out.println("La disivion de " + a + " / " + b + " = " + basica.division(a, b));
+                break;
 
-        Integer resultadoMultiplicacion = basica.multiplicacion(a, b);
-        System.out.println("La multiplicacion de " + a + " * " + b + " = " + basica.multiplicacion(a,b));
+            case multiplicacion:
+                System.out.println("La multiplicacion de " + a + " * " + b + " = " + basica.multiplicacion(a, b));
+                break;
+        }
     }
 }
